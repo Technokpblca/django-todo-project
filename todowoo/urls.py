7b3961d396ecc27a1107411d822fefb76609b95b
Abbat <misochka-piva@yandex.ru>
2022-07-08 13:22:37 +0300

    Complete authentification

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
     # Todos
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
 ]
